ea4a27ecc375956926fb6deb8bbce2c783fd6554
Sos247 <stavvals@yahoo.com>
2023-03-11 14:49:22 +0200

    fixed selectClass(), επρεπε οι τιμες στα hp,armor κλπ να μην ειναι μηδενικες για το return. public int encounter() -> public Enemy encounter(), γυρναει enemy οποτε θα μπουνε και τα itmes στο pool σε 2ο χρονο

--- Combat System/src/Adventure.java
+++ Combat System/src/Adventure.java
@@ -5,51 +5,19 @@ import java.util.Scanner;
 public class Adventure {
 
     public Hero hero;
+    public Enemy enemy;
 
     int encounterRoll;
-    Items item ;
+    Items item;
 
     private int steps;
 
     public void init() {
 
+        setDifficulty();
         hero = selectClass();
+        initialize(hero, enemy);
     }
-
-    public Hero selectClass() {
-
-        int selection;
-
-        Hero hero;
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Select Your Class \n 1.Barbarian \n 2.Rogue \n 3.Wizard");
-
-        System.out.print("Choice : ");
-
-        do {
-            selection = input.nextInt();
-            switch (selection) {
-                case 1:
-                    hero = new Hero(NPCType.BARBARIAN, "", 0, 0, 0, 0);
-                    break;
-                case 2:
-                    hero = new Hero(NPCType.ROGUE, "", 0, 0, 0, 0);
-                    break;
-                case 3:
-                    hero = new Hero(NPCType.WIZARD, "", 0, 0, 0, 0);
-                    break;
-                default:
-                    System.out.print("Select a Proper value : ");
-                    
-                    break;
-            }
-        } while (selection > 3 || selection < 1);
-
-        return hero;
-    }
-
     public void setDifficulty() {
         int difficulty;
         Scanner input = new Scanner(System.in);
@@ -78,37 +46,64 @@ public class Adventure {
     public int getDifficulty() {
         return steps;
     }
+    public Hero selectClass() {
 
+        int selection;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Select Your Class \n 1.Barbarian \n 2.Rogue \n 3.Wizard");
+
+        System.out.print("Choice : ");
+
+        do {
+            selection = input.nextInt();
+            switch (selection) {
+                case 1:
+                    hero = new Hero(NPCType.BARBARIAN, "Barbarian", 100, 40, 60, 60);
+                    break;
+                case 2:
+                    hero = new Hero(NPCType.ROGUE, "Rogue", 100, 30, 45, 80);
+                    break;
+                case 3:
+                    hero = new Hero(NPCType.WIZARD, "Wizard", 100, 80, 30, 45);
+                    break;
+                default:
+                    System.out.print("Select a Proper value : ");
+                    break;
+            }
+        } while (selection > 3 || selection < 1);
+
+        return hero;
+    }
     public void initialize(Hero h, Enemy e) {
-        ArrayList<Integer> path = new ArrayList<>();
+        ArrayList<Enemy> path = new ArrayList<>();
         for (int i = 1; i < getDifficulty(); ) {
             path.add(encounter());
             if (encounterRoll >= 0 && encounterRoll <= 2) {
-              battle(h, e);
+                //battle(h, e);
             } else {
                 i++;
             }
         }
     }
-    public int encounter() {
-        NPC.classes enemy;
+
+    public Enemy encounter() {
+
         Random randSelect = new Random();
         encounterRoll = randSelect.nextInt(6);
         switch (encounterRoll) {
             case 0:
                 System.out.print("You Encounter a ");
-                enemy = NPC.classes.BARBARIAN;
-                enemy.allStatsTogether();
+                enemy = new Enemy(NPCType.BARBARIAN, "Barbarian", 100, 40, 60, 60);
                 break;
             case 1:
                 System.out.print("You Encounter a ");
-                enemy = NPC.classes.ROGUE;
-                enemy.allStatsTogether();
+                enemy = new Enemy(NPCType.ROGUE, "Rogue", 100, 30, 45, 80);
                 break;
             case 2:
                 System.out.print("You Encounter a ");
-                enemy = NPC.classes.WIZARD;
-                enemy.allStatsTogether();
+                enemy = new Enemy(NPCType.WIZARD, "Wizard", 100, 80, 30, 45);
                 break;
             case 3:
                 System.out.println("You Encountered Nothing");
@@ -117,14 +112,14 @@ public class Adventure {
                 item.itemDrop();
                 break;
         }
-        return encounterRoll;
+        return enemy;
     }
 
-    public void battle(Hero h, Enemy e) {
-        do {
-            h.heroAttack();
-            e.enemyAttack();
-
-        } while (h.getHp() > 0 && e.getHp() > 0);
-    }
+//    public void battle(Hero h, Enemy e) {
+//        do {
+//            h.heroAttack();
+//            e.enemyAttack();
+//
+//        } while (h.getHp() > 0 && e.getHp() > 0);
+//    }
 }
